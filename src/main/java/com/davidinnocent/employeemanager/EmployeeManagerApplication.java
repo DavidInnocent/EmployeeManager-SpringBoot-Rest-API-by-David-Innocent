090d8dc9ead389dafd2b29393c68d6ddb0df214b
David Innocent <davidinnocent@live.com>
2021-04-18 19:06:31 +0300

    added cors configuration

--- src/main/java/com/davidinnocent/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/davidinnocent/employeemanager/EmployeeManagerApplication.java
@@ -1,7 +1,14 @@
 package com.davidinnocent.employeemanager;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeeManagerApplication {
@@ -9,5 +16,22 @@ public class EmployeeManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagerApplication.class, args);
     }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration configuration=new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:ANGULARPORT HERE"));
+        configuration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
+                "Accept","Authorization","Origin, Accept","X-Requested-With",
+                "Access-Control-Request-Method","Access-Control-Request-Headers"));
+        configuration.setExposedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
+                "Accept","Authorization","Access-Control-Allow-Credentials"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource=new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",configuration);
+        return new org.springframework.web.filter.CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
 
 }
